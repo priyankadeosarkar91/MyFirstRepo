7675c3da42a140d446d4efcb2c5f321482450bb7
priyankadeosarkar91 <priyadeosarkar91@gmail.com>
2020-03-15 20:36:58 +0530

    First Commit

---
+++ src/Demo1/test1.java
@@ -0,0 +1,13 @@
+package Demo1;
+
+public class test1 {
+	public static void main(String[] args) {
+		System.out.println("Hello, Priyanka");
+		System.out.println("Hello, Priyanka Deosarkar");
+		int a=5,b=5,c;
+		c=a+b;
+		System.out.println(c);
+		
+	}
+ 
+}
